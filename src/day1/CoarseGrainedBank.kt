66a120e0ffc3918fbde2a802f010df047aa440f6
Vladislav Annenkov <vladislav.annenkov@jetbrains.com>
2025-06-24 23:11:01 +0200

    [day 1] Solve `CoarseGrainedBank`

--- src/day1/CoarseGrainedBank.kt
+++ src/day1/CoarseGrainedBank.kt
@@ -3,47 +3,62 @@
 package day1
 
 import day1.Bank.Companion.MAX_AMOUNT
-import java.util.concurrent.locks.*
+import java.util.concurrent.locks.ReentrantLock
 
 class CoarseGrainedBank(accountsNumber: Int) : Bank {
     private val accounts: Array<Account> = Array(accountsNumber) { Account() }
 
-    // TODO: use this mutex to protect all bank operations.
     private val globalLock = ReentrantLock()
 
     override fun getAmount(id: Int): Long {
-        // TODO: Make this operation thread-safe via coarse-grained locking.
-        return accounts[id].amount
+        globalLock.lock()
+        try {
+            return accounts[id].amount
+        } finally {
+            globalLock.unlock()
+        }
     }
 
     override fun deposit(id: Int, amount: Long): Long {
-        // TODO: Make this operation thread-safe via coarse-grained locking.
-        require(amount > 0) { "Invalid amount: $amount" }
-        val account = accounts[id]
-        check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        account.amount += amount
-        return account.amount
+        globalLock.lock()
+        try {
+            require(amount > 0) { "Invalid amount: $amount" }
+            val account = accounts[id]
+            check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
+            account.amount += amount
+            return account.amount
+        } finally {
+            globalLock.unlock()
+        }
     }
 
     override fun withdraw(id: Int, amount: Long): Long {
-        // TODO: Make this operation thread-safe via coarse-grained locking.
-        require(amount > 0) { "Invalid amount: $amount" }
-        val account = accounts[id]
-        check(account.amount - amount >= 0) { "Underflow" }
-        account.amount -= amount
-        return account.amount
+        globalLock.lock()
+        try {
+            require(amount > 0) { "Invalid amount: $amount" }
+            val account = accounts[id]
+            check(account.amount - amount >= 0) { "Underflow" }
+            account.amount -= amount
+            return account.amount
+        } finally {
+            globalLock.unlock()
+        }
     }
 
     override fun transfer(fromId: Int, toId: Int, amount: Long) {
-        // TODO: Make this operation thread-safe via coarse-grained locking.
-        require(amount > 0) { "Invalid amount: $amount" }
-        require(fromId != toId) { "fromIndex == toIndex" }
-        val from = accounts[fromId]
-        val to = accounts[toId]
-        check(amount <= from.amount) { "Underflow" }
-        check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        from.amount -= amount
-        to.amount += amount
+        globalLock.lock()
+        try {
+            require(amount > 0) { "Invalid amount: $amount" }
+            require(fromId != toId) { "fromIndex == toIndex" }
+            val from = accounts[fromId]
+            val to = accounts[toId]
+            check(amount <= from.amount) { "Underflow" }
+            check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
+            from.amount -= amount
+            to.amount += amount
+        } finally {
+            globalLock.unlock()
+        }
     }
 
     /**
