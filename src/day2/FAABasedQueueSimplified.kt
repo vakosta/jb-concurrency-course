6f95b84ef463ae563474c244f7bfb2b66c089be1
Vladislav Annenkov <vladislav.annenkov@jetbrains.com>
2025-08-03 18:17:57 +0200

    [day 2] Solve `FAABasedQueueSimplified`

--- src/day2/FAABasedQueueSimplified.kt
+++ src/day2/FAABasedQueueSimplified.kt
@@ -1,8 +1,10 @@
 package day2
 
-import day1.*
-import java.util.concurrent.atomic.*
-import kotlin.math.*
+import day1.Queue
+import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.atomic.AtomicReferenceArray
+import kotlin.math.max
+import kotlin.math.min
 
 class FAABasedQueueSimplified<E> : Queue<E> {
     private val infiniteArray = AtomicReferenceArray<Any?>(1024) // conceptually infinite array
@@ -10,26 +12,38 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     private val deqIdx = AtomicLong(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.get()
-        enqIdx.set(i + 1)
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray.set(i.toInt(), element)
+        while (true) {
+            val i = enqIdx.getAndAdd(1).toInt()
+            if (infiniteArray.compareAndSet(i, null, element))
+                return
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.get() <= deqIdx.get()) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.get()
-        deqIdx.set(i + 1)
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray.get(i.toInt()) as E
+        while (true) {
+            if (!shouldTryToDequeue())
+                return null
+
+            val i = deqIdx.getAndAdd(1).toInt()
+            if (infiniteArray.compareAndSet(i, null, POISONED))
+                continue
+
+            val element = infiniteArray[i] as E
+            infiniteArray[i] = null
+            return element
+        }
+    }
+
+    private fun shouldTryToDequeue(): Boolean {
+        while (true) {
+            val curEnq = enqIdx.get()
+            val curDeq = deqIdx.get()
+
+            if (curEnq != enqIdx.get())
+                continue
+            return curEnq > curDeq
+        }
     }
 
     override fun validate() {
